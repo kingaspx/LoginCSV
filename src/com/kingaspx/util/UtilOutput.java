13a3fc7805a6154e915939e3b895a934aab28898
Abner Rodrigues <abnerodrigs@gmail.com>
2019-07-28 13:07:26 -0300

    initial

---
+++ src/com/kingaspx/util/UtilOutput.java
@@ -0,0 +1,39 @@
+package com.kingaspx.util;
+
+import javax.swing.JOptionPane;
+
+public class UtilOutput {
+
+    public static void messageBox(String str) {
+        JOptionPane.showMessageDialog(null, str);
+    }
+
+    public static void messageBox(int str) {
+        JOptionPane.showMessageDialog(null, str);
+    }
+
+    public static void messageBox(double str) {
+        JOptionPane.showMessageDialog(null, str);
+    }
+
+    public static void messageBox(float str) {
+        JOptionPane.showMessageDialog(null, str);
+    }
+
+    public static void log(String str) {
+        System.out.println(str);
+    }
+
+    public static void log(int str) {
+        System.out.println(str);
+    }
+
+    public static void log(double str) {
+        System.out.println(str);
+    }
+
+    public static void log(float str) {
+        System.out.println(str);
+    }
+
+}
